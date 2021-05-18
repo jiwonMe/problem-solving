ec3b56f98c6836a529d3e41ee56af32eebee458e
Jiwon Park <park@jiwon.me>
2021-05-18 12:55:01 +0900

    INIT

---
+++ acmicpc/1000/1000.c
@@ -0,0 +1,9 @@
+/* www.acmicpc.net/problem/1000
+ *  Jiwon Park 2021 (c)
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world");
+}
---
+++ acmicpc/starterpack.sh
@@ -0,0 +1,12 @@
+# !sh
+mkdir $1
+echo "/* www.acmicpc.net/problem/$1
+ *  Jiwon Park 2021 (c)
+ */
+#include <stdio.h>
+
+int main(){
+
+}
+" > ./$1/$1.c
+
