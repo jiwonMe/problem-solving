e50234f0e038118043edcec1378ea49e425dee1c
Jiwon Park <park@jiwon.me>
2021-07-04 20:01:44 +0900

    solved: solved.ac class 1

--- acmicpc/11720/11720.c
+++ acmicpc/11720/11720.c
@@ -8,7 +8,14 @@
 
 int main()
 {
-
+    int n, sum = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+        scanf("%1d", &tmp);
+        sum += tmp;
+    }
+    printf("%d", sum);
 }
 /* END */
-
